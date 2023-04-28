41e1db07f2cb273b408639591185da40b94bba26
mohit <rajpootmohit782@gmail.com>
2023-04-28 17:37:14 +0000

    using (express.js) Update 4 files

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-const http = require("http");
-const fs = require("fs");
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write("<body>");
-    res.write("<form action='/message' method='POST'>"); // Adding a form element
-    res.write("<input type='text' name='message'>"); // Adding an input field
-    res.write("<button type='submit'>Submit</button>"); // Adding a button
-    res.write("</form>");
-    res.write("</body>");
-    res.write("</html>");
-    return res.end();
-  }
+const express = require("express");
 
-  if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt");
-    res.statusCode = 302;
-    res.setH    res.writeHead(302, { "Location", "/" });
-    res.write("Welcome to my Node Js project");
-    return res.end();
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.write("Page not found");
-    res.end();
-  }
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("middware 1");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("middware 23");
+  res.send("<h1>hiiii</h1/>")
 });
 
-server.listen(4000, () => {
-  console.log("Server running at http://localhost:4000/");
-});
+app.listen(4000);
