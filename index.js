81cd768edc6b9dc35d5d3548dee4d842caa20696
mohit <rajpootmohit782@gmail.com>
2023-04-29 06:30:06 +0000

    post express Update 4 files

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("middware 1");
+app.use(bodyParser.urlencoded());
+app.use("/", (req, res, next) => {
+  console.log("middware 100");
+  //res.send("<h1>always runs</h1/>");
   next();
 });
-app.use((req, res, next) => {
+
+app.use("/add-products", (req, res, next) => {
+  console.log("add-products");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' placeHolder='title' name='title'/> <input type='text' placeHolder='size' name='size'/><button type='submit'>Add products</button></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body.title, req.body.size);
+  res.redirect("/");
+});
+app.use("/", (req, res, next) => {
   console.log("middware 23");
-  res.send("<h1>hiiii</h1/>")
+  res.send("<h1>front-page</h1/>");
 });
 
 app.listen(4000);
